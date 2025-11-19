701869deebb6fbe1b9ef8be08b322320ebf1ce52
Salma <salmazidan1164@gmail.com>
2025-11-20 01:55:36 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,12 @@ function App() {
 
   return (
     <>
-      {" "}
-      <h1 className="text-3xl font-bold test-blue underline">Hello world!</h1>
-    </>
+          <div className="h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-indigo-600 hover:text-red-500 transition-all">
+        Tailwind is working! 🎉
+      </h1>
+    </div>
+</>
   );
 }
 
