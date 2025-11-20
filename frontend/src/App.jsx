c37eafe54aaa86368b38c7500cc5e3af0eeb752a
Salma <salmazidan1164@gmail.com>
2025-11-20 15:13:53 +0200

    updated folders structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
           <div className="h-screen flex items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-bold text-indigo-600 hover:text-red-500 transition-all">
         Tailwind is working! 🎉
+        goood
       </h1>
     </div>
 </>
