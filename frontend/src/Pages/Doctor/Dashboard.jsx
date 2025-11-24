a70aa6f0f16c835bfb20e9b07e24ffd7bb70caa3
Salma <salmazidan1164@gmail.com>
2025-11-25 00:07:25 +0200

    completed the Requests section in Doctor dashboard

--- frontend/src/Pages/Doctor/Dashboard.jsx
+++ frontend/src/Pages/Doctor/Dashboard.jsx
@@ -4,16 +4,19 @@ import PrimButton from "../../components/Common/PrimButton";
 import EarningsChart from "../../components/Doctor/EarningsChart";
 import { Bar } from "recharts";
 import BarChar from "../../components/Doctor/BarChar";
+import Calendar from "../../components/Doctor/Calendar";
+import RequestCard from "@/components/Doctor/RequestCard";
+// import avatar from '../../assests/avatar.svg'
 
 function Dashborad() {
   const cardStyles =
     "flex flex-col justify-start items-center gap-4 p-6 w-80 h-48 relative";
 
   return (
-    <>
+    <div className="flex flex-col justify-center p-20 gap-10">
 
-    {/* the first section */}
-      <div className="flex gap-10 justify-center items-center mt-20 p-10 rounded-2xl">
+      {/* the first section */}
+      <div className="flex gap-10 justify-center items-center rounded-2xl">
         <Card classname={cardStyles}>
           <div className="absolute top-5 left-5">
             <Card classname="w-9 h-9 rounded-full "></Card>
@@ -40,8 +43,7 @@ function Dashborad() {
         </Card>
       </div>
 
-
-    {/* the charts section */}
+      {/* the charts section */}
       <div className="flex gap-10 justify-center items-center ">
         <Card
           classname={`h-64 flex flex-col justify-start items-center gap-4 p-6 w-80 relative`}
@@ -49,15 +51,15 @@ function Dashborad() {
           <div className="absolute top-5 left-5">
             <Card classname="w-9 h-9 rounded-full "></Card>
           </div>
-          <h1 className="font-semibold text-2xl text-center ml-5 mt-[-2px] ">New vs Returning Patients</h1>
+          <h1 className="font-semibold text-2xl text-center ml-5 mt-[-2px] ">
+            New vs Returning Patients
+          </h1>
 
           <div className="absolute bottom-1 left-0 w-72 h-44">
             <BarChar />
           </div>
         </Card>
-        <Card
-          classname={`h-64 ${cardStyles}`}
-        >
+        <Card classname={`h-64 ${cardStyles}`}>
           <div className="absolute top-5 left-5">
             <Card classname="w-9 h-9 rounded-full "></Card>
           </div>
@@ -80,7 +82,25 @@ function Dashborad() {
           </div>
         </Card>
       </div>
-    </>
+
+      {/* the third section Calendar */}
+      <div className="flex justify-center gap-16 items-center ">
+        <Card>
+          <Calendar />
+        </Card>
+
+        <Card classname={`h-[414px] w-[510px] relative flex flex-col justify-start items-center gap-2 p-6 `}>
+          <div className="absolute top-5 left-5">
+            <Card classname="w-9 h-9 rounded-full "></Card>
+          </div>
+          <h1 className="font-semibold text-2xl mb-4">Requests</h1>
+          <RequestCard />
+          <RequestCard />
+          <RequestCard />
+          <RequestCard />
+        </Card>
+      </div>
+    </div>
   );
 }
 
