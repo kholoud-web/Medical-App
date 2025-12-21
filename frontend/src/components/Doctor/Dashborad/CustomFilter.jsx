678f53eeeb3d1d42dd7e7e408169333ae68c9054
Salma <salmazidan1164@gmail.com>
2025-12-21 03:29:15 +0200

    Doctor Dashborad done

--- frontend/src/components/Doctor/Dashborad/CustomFilter.jsx
+++ frontend/src/components/Doctor/Dashborad/CustomFilter.jsx
@@ -3,21 +3,22 @@ import Card from "@/components/Common/Card";
 
 function CustomFilter({ options = []  , handleClick}) {
   return (
-    <Card classname="flex gap-2 ">
-      {options.map((option, index) =>
-        option.active ? (
-          <Card key={index} onClick={()=>handleClick(option)} classname="!bg-[#C6D8FD] text-[#3562BC] font-semibold w-28 text-center border-collapse">
-            <button>
-                {option.title}
-            </button>
-          </Card>
-        ) : (
-          <button key={index} onClick={()=>handleClick(option)} className="font-semibold w-28 text-center text-[#8F8F8F]">
-            {option.title}
-          </button>
-        )
-      )}
-    </Card>
+<div className="flex gap-2">
+      {options.map((option, index) => (
+        <button
+          key={index}
+          onClick={() => handleClick(option)}
+          className={`px-4 py-1 w-32 text-sm rounded-full transition-colors
+            ${
+              option.active
+                ? "bg-blue-500 text-white"
+                : "border border-blue-500 text-blue-500 hover:bg-blue-50"
+            }`}
+        >
+          {option.title}
+        </button>
+      ))}
+    </div>
   );
 }
 
