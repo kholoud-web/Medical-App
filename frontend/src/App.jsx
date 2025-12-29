d00baea5bc943d2c3cac30166688fff5dfd06c6a
Nedaa Elsayed <nedaaelsayed321@gmail.com>
2025-12-29 22:11:48 +0200

    api register is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ import FAQ from "./Pages/Common/FAQ/FAQ";
 import Finance from "./Pages/Doctor/Finance/Finance";
 import MedicalFiles from "./Pages/Doctor/MedicalFiles/MedicalFiles";
 import SuggestedTreatments from "./Pages/Patient/SuggestedTreatments/SuggestedTreatments";
-import PatientProfile from "./Pages/Admin/PatientProfile";
+import PatientProfile from "./Pages/Patient/PatientProfile/PatientProfile";
 import DoctorProfile from "./Pages/Admin/DoctorProfile/DoctorProfile";
 import SystemSetting from "./Pages/Admin/SystemSetting/SystemSetting";
 import DoctorsManagement from "./Pages/Admin/DoctorsManagement/DoctorsManagement";
@@ -42,13 +42,11 @@ import ResetPassword from './Pages/Auth/ResetPassword';
 import ResetSuccess from "./Pages/Auth/ResetSuccess";
 import Inquiries from "./Pages/Doctor/Inquiries/Inquiries";
 import PatientDashboard from "./Pages/Patient/Dashboard/PatientDashboard";
-import Consultations from "./Pages/Doctor/Consultation/Consultations";
-import PatientsManagement from "./Pages/Admin/PatientTable/PatientsManagement";
 
 function App() {
-  const role = "doctor";
+  const role = "patient";
   const helpElement = role === "patient" ? <PatientHelp /> : <HelpSupport />;
-  // const settingsElement = role === "doctor" ? <DoctorSettings /> : <PatientSettings />;
+  const settingsElement = role === "doctor" ? <DoctorSettings /> : <PatientSettings />;
 
   const router = createBrowserRouter([
     { path: "login", element: <Login /> },
@@ -80,7 +78,6 @@ function App() {
             { path: "appointments", element: <Appointments /> },
             { path: "dashboard", element: <Dashboard /> },
             { path: "diagnosis", element: <DiagnosisAssistant /> },
-            { path: "Consultation", element: <Consultations /> },
             { path: "treatment", element: <Treatment /> },
             { path: "my-patients", element: <MyPatients /> },
             { path: "reports", element: <Reports /> },
@@ -93,10 +90,7 @@ function App() {
             ? [
               { path: "notificationCenter", element: <NotificationCenter /> },
               
-              { path: "drugChecker", element: <DrugChecker /> },
               { path: "doctorsManagement", element: <DoctorsManagement /> },
-              { path: "PatientsManagement", element: <PatientsManagement /> }
-
 
             ]
           : role === "patient"
@@ -112,15 +106,11 @@ function App() {
             ]
           : []),
         {
-          path: "settings", element: <settingsElement/>
+          path: "settings", element: settingsElement
         },
         {
           path: "help", element: helpElement
         },
-        {
-          path:"helpSupport", element:<HelpSupport/>
-        },
-
         {
           path: "MedicalFiles", element: <MedicalFiles />
         },
--- frontend/src/RiduxToolkit/Store.js
+++ frontend/src/RiduxToolkit/Store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import drugCheckerReducer from "./Slices/DrugCheckerSlice";
+import authReducer from './Slices/authSlice'
+
 
 export const store = configureStore({
   reducer: {
     drugChecker: drugCheckerReducer,
+    auth: authReducer,
   },
 });
