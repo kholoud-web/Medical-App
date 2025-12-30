53924de030f0ad70ff925c0def89efa58a0b1100
kholoud <kholudmahmoud62@gmail.com>
2025-12-30 09:14:58 +0200

    create inquiry slice

--- frontend/src/RiduxToolkit/Store.js
+++ frontend/src/RiduxToolkit/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import drugCheckerReducer from "./Slices/DrugCheckerSlice";
 import authReducer from './Slices/authSlice'
-
+import inquiryReducer from './Slices/inquirySlice.js'
+ 
 
 export const store = configureStore({
   reducer: {
     drugChecker: drugCheckerReducer,
     auth: authReducer,
+    inquiry:inquiryReducer,
   },
 });
