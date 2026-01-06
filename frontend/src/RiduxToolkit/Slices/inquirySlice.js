0f74570a69b059057a2714e5a8332de946ba7113
kholoud <kholudmahmoud62@gmail.com>
2026-01-06 16:14:13 +0200

    help slice

--- frontend/src/RiduxToolkit/Slices/inquirySlice.js
+++ frontend/src/RiduxToolkit/Slices/inquirySlice.js
@@ -1,5 +1,3 @@
-import { API } from "@/Api/Api";
-import Inquiries from "@/Pages/Doctor/Inquiries/Inquiries";
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
