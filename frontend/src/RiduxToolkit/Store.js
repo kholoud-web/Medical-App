45135d7cbe4e3cf11927eb07fa36378633dc27a6
Salma Abdelaziz <93342442+Salmaabdelaziz271@users.noreply.github.com>
2026-01-12 21:48:02 +0200

    add doctorManagement and system setting APIs

--- frontend/src/RiduxToolkit/Store.js
+++ frontend/src/RiduxToolkit/Store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import drugCheckerReducer from "./Slices/DrugCheckerSlice";
 import authReducer from './Slices/authSlice'
 import consultationReducer from './Slices/ConsultationSlice';
-import inquiryReducer from './Slices/InquirySlice'
+import inquiryReducer from './Slices/inquirySlice'
 import modifyConsultationReducer from "./Slices/modifyConsultationSlice";
 import helpReducer from './Slices/HelpSupportSlice'
+import doctorManagementReducer from './Slices/DoctorManagementSlice'
+import systemSettingReducer from './Slices/SystemSettingSlice'
 
 export const store = configureStore({
   reducer: {
@@ -14,5 +16,7 @@ export const store = configureStore({
     modifyConsultation: modifyConsultationReducer,
     inquiry:inquiryReducer,
     help:helpReducer,
+    doctorManagement: doctorManagementReducer,
+    systemSetting: systemSettingReducer,
   },
 }); 
